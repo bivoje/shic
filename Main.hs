cec61313efee1e69f5e74821a78a7bd5f3e6d711
bivoje <bivoje49@gist.ac.kr>
2018-01-16 13:56:58 +0900

    Decrease memory size in test code

--- Main.hs
+++ Main.hs
@@ -12,7 +12,7 @@ import Cpu.Debugger
 main :: IO ()
 main = do
     runAssembler "sample4.asm"
-    let clean_mem = fixedMemory 2000
+    let clean_mem = fixedMemory 350
     (mem, pcv) <- loadFile "sample4.obj" clean_mem
     let st = setR pc pcv $ cpuState mem []
     (ST mem reg _) <- runCpu st
