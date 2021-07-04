c745cbc74cc83c2817e1947bddc791d9804ae8ab
bivoje <bivoje49@gmail.com>
2021-07-04 15:42:09 +0900

    Enable emulator/debugger to accepts asm filepath through arg

--- app/Debugger/Debugger.hs
+++ app/Debugger/Debugger.hs
@@ -2,8 +2,10 @@
 module Main where
 
 import System.IO
+import System.Environment
 import qualified Data.ByteString as B
 import Data.List (dropWhileEnd)
+import Control.Monad (void)
 
 import Assembler (assembler)
 import Loader
@@ -12,24 +14,29 @@ import Cpu
 import Cpu.Types
 import Cpu.Debugger
 
-runAssembler :: FilePath -> IO ()
+runAssembler :: FilePath -> IO FilePath
 runAssembler filepath = do
     contents <- B.readFile filepath
     let outfile = dropWhileEnd (/='.') filepath ++ "obj"
     let objxt = assembler contents
     B.writeFile outfile objxt
+    return outfile
 
 runLoader :: FilePath -> Memory -> IO (Memory, Address)
 runLoader filepath mem = flip loader mem <$> B.readFile filepath
 
 main :: IO ()
 main = do
-    runAssembler "sample4.asm"
-    let clean_mem = fixedMemory 2000
-    (mem, pcv) <- runLoader "sample4.obj" clean_mem
-    let ds = [(6, Device NullIn (Dump "dev6"))]
+    args <- getArgs
+    src <- case args of
+        [src] -> return src
+        _ -> fail "Specify exactly one source as an argument"
+    tar <- runAssembler src
+    let clean_mem = fixedMemory 2000 -- FIXME make it adjustable
+    (mem, pcv) <- runLoader tar clean_mem
+    let ds = map (\i -> (i, Device ForwardIn (Dump $ "dev" ++ show i))) [0..10] -- FIXME make it adjustable
     let st = setR pc pcv $ cpuState mem emptyRegister ds
-    (ST mem reg _) <- debugCpu st
+    (ST mem reg _) <- debugCpu st 
     print mem
     print reg
-    return ()
+
